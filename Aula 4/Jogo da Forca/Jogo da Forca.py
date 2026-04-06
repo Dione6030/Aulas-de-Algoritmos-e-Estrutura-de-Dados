9527845ff391b69d9c1faedf4a44b19c88c80e86
Dione Pinheiro <dionepinheiro6030@gmail.com>
2026-04-06 16:06:00 -0300

    ✨ Inicia o Jogo da forca

---
+++ Aula 4/Jogo da Forca/Jogo da Forca.py
@@ -0,0 +1,132 @@
+import random
+import time
+import os
+import subprocess
+
+palavras = []
+dicas = []
+
+def limpa_tela():
+    if os.name == 'nt':
+        subprocess.run('cls', shell=True)
+    else:
+        subprocess.run('clear', shell=True)
+
+def carrega_dados():
+    if not os.path.isfile("palavras.txt"):
+        print("Nenhum arquivo contendo as palavras foi encontrado.")
+        return
+    else:
+        print("Rastreando palavras.")
+    
+    with open("palavras.txt", "r") as arq:
+        dados = arq.readlines()
+        
+        for linha in dados:
+            linha = linha.strip()
+            if not linha:
+                continue
+            
+            partes = linha.split(";", 1)
+            if len(partes) == 2:
+                palavras.append(partes[0])
+                dicas.append(partes[1])
+
+carrega_dados()
+
+print("="*40)
+print("Jogo da Forca")
+print("="*40)
+
+jogador = input("\nNome do Jogador: ")
+
+def desenha_forca(erros):
+    limpa_tela()
+    cabeca = "O" if erros >=1 else " "
+    corpo = "|" if erros >= 2 else " "
+    braco_esq = "/" if erros >= 3 else " "
+    braco_dir = "\\" if erros >= 4 else " "
+    perna_esq = "/" if erros >= 5 else " "
+    perna_dir = "\\" if erros >= 6 else " "
+
+    print(f"""
+     |---|
+     |   {cabeca}
+     |  {braco_esq}{corpo}{braco_dir}
+     |  {perna_esq} {perna_dir}
+    ---
+    """)
+    print("\n")
+
+def palavra_aleatoria():
+    aleatorio = random.randint(0, len(palavras)-1)
+    return palavras[aleatorio], dicas[aleatorio]
+
+palavra, dica = palavra_aleatoria()
+
+
+def mostra_palavra(palavra_oculta, letras_erradas, erros):
+    if erros >= 4:
+        print(f"Dica: {dica}")
+    
+    print("Palavra: ", " ".join(palavra_oculta))
+    print("Letras Erradas: ", " ".join(letras_erradas))
+    print("\n")
+
+def faz_aposta():
+    erros = 0
+    palavra_oculta = ["_" for _ in palavra]
+    letras_erradas = []
+    
+    while True:
+        limpa_tela()
+        desenha_forca(erros)
+        mostra_palavra(palavra_oculta, letras_erradas, erros)
+        
+        aposta = input("Digite uma letra ou a palavra completa: ").strip().lower()
+        
+        if not aposta.isalpha():
+            print("Aposta inválida! Digite apenas letras.")
+            time.sleep(2)
+            continue
+        try:
+            if aposta in palavra:
+                for i, letra in enumerate(palavra):
+                    if letra == aposta:
+                        palavra_oculta[i] = aposta
+                break
+            else:
+                if aposta not in letras_erradas:
+                    letras_erradas.append(aposta)
+                    erros += 1
+                    break
+                else:
+                    print("Está letra já foi, tente outra!")
+                    time.sleep(2)
+                    break
+        except IndexError:
+            print("Letra ou palavra invalida, Repita!")
+            time.sleep(2)
+    return erros, palavra_oculta, letras_erradas
+
+def verifica_palavra(palavra_oculta):
+    faltam = palavra_oculta.count("_")
+    return faltam
+
+
+mostra_palavra()
+
+################################ Programa Principal ###############################
+while True:
+    aposta = faz_aposta()
+    
+    if aposta[1] in palavra:
+        print("Parabéns, você acertou alguma letra! 🥳")
+        contador = verifica_palavra(aposta[1])
+        time.sleep(2)
+        if contador == 0:
+            print("Parabéns! Você venceu! 🎉🎉")
+            break
+        else:
+            print(f"Faltam descobrir: {contador} letra(s)")
+        time.sleep(2)
