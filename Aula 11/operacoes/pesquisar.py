e6c35dc73564c8cdcc274ccca845a3b5a763f0fd
Dione F. <dionepinheiro6030@gmail.com>
2026-06-15 19:22:47 -0300

    🎉 Atualiza função de pesquisa para incluir plataformas e exibir detalhes do filme

--- Aula 11/operacoes/pesquisar.py
+++ Aula 11/operacoes/pesquisar.py
@@ -3,26 +3,28 @@ import requests
 from rich.console import Console
 from rich.panel import Panel
 
-from config import API_URL_FILME
+from config import API_URL_FILME, API_URL_PLATAFORMA
 
 def pesquisar():
     id = input("ID do filme: ")
 
     resposta_filme = requests.get(f"{API_URL_FILME}/{id}")
+    resposta_plataformas = requests.get(API_URL_PLATAFORMA)
 
     if resposta_filme.status_code != 200:
         print("Filme não encontrado")
         return
 
-    selecao = resposta_filme.json()
+    filme = resposta_filme.json()
+    plataformas = resposta_plataformas.json()
     console = Console()
 
     texto = (
-        f"Nome: {selecao['nome']}\n"
-        f"Continente: {selecao['continente']}\n"
-        f"Copas: {selecao['numCopas']}\n"
-        f"Destaques: {selecao['destaques']}\n"
-        f"Chance de Título: {selecao['chanceTitulo']}%"
+        f"Nome: {filme['nome']}\n"
+        f"Gênero: {filme['genero']}\n"
+        f"Duração: {filme['duracao']} minutos\n"
+        f"Sinopse: {filme['sinopse']}\n"
+        f"Chance de Título: {filme['chanceTitulo']}%"
     )
 
     console.print(
